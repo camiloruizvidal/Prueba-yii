6382cb1428ebfdb8e8fba68f19d54661215cc98d
camilo ruiz <camiloruizvidal@gmail.com>
2020-09-23 23:22:32 -0500

    redirect

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -1,5 +1,5 @@
 export default [
-	{ path: '/', component: () => import(/* webpackChunkName: "inicio" */ '../pages/Initial.vue') },
+	{ path: '/', redirect: '/productos' },
 	{ path: '/productos', component: () => import(/* webpackChunkName: "productos" */ '../pages/Index.vue') },
 	{ path: '/productos/crear', component: () => import(/* webpackChunkName: "crear-productos" */ '../pages/Create.vue') },
 	{ path: '/productos/editar/:id', component: () => import(/* webpackChunkName: "editar-productos" */ '../pages/Edit.vue') },
